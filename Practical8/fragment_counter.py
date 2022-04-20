498888bff444c408cbc2f33bef1ff93e9fb240dd
resffffff <101092328+resffffff@users.noreply.github.com>
2022-04-20 11:44:57 +0800

    debug version

--- Practical8/fragment_counter.py
+++ Practical8/fragment_counter.py
@@ -1,4 +1,5 @@
-gene ='ATGCAATCGACTACGATCAATCGAGGGCC'  # Now the 'gene' is opened and read as a sequence
-gene_seq = re.findall(r'GAATTC', gene)  # We create a gene list containing 'GAATTC'
+import re
+gene ='ATGCAATCGACTACGATCAATCGAGGGCC'  # Now the 'gene' is defined and read as a sequence
+gene_seq = re.findall(r'GAATTC', gene)  # We create a gene list to count the appearing times of 'GAATTC'
 print(len(gene_seq) + 1)   # We print out the amount of gene that containing the specific gene segment.
 
