a0fbcd519c1f3a4ae046fcf6a21a44e1294bd562
resffffff <101092328+resffffff@users.noreply.github.com>
2022-05-24 21:27:41 +0800

    second version

--- Practical6/Paternal age vs offspring health.py
+++ Practical6/Paternal age vs offspring health.py
@@ -1,13 +1,37 @@
-#     create a dictionary firstly, and then create the plot using "plt.scatter"
+# Pseudocode:
+# input the paternal_age data list and the chd data list
+# create the dictionary
+# invoke plt.scatter to create a plot
+# set the marker, colour and size of the dots
+# output the plot
+# input the father's age
+# use while loop to find CHD in 'chd' list
+# output the CHD risk in the offspring
 import numpy as np
 import matplotlib.pyplot as plt
 
-AGE = [30,35,40,45,50,55,60,65,70,75]
-HEALTH = [1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
-dictionary = {'30': 1.03,'35': 1.07,'40': 1.11,'45': 1.17,'50': 1.23,'55': 1.32,'60': 1.42,'65': 1.55,'70': 1.72,'75':1.94}
-print(dictionary) #Output a	dictionary in python containing the	information	in the two lists above
-plt.scatter(list(dictionary.keys()),list(dictionary.values()))
+paternal_age = [30,35,40,45,50,55,60,65,70,75] # create a list containing paternal ages
+chd = [1.03, 1.07, 1.11, 1.17, 1.23, 1.32, 1.42, 1.55, 1.72, 1.94]
+# creat the dictionary
+dictionary = dict(zip(paternal_age,chd)) # Output a dictionary containing relative informations in the 2 list
+print(dictionary)
+plt.scatter(list(dictionary.keys()),list(dictionary.values()),
+        marker = 'o',
+        c = 'red', # the colour of dots are set as red
+        s = 50 # the radius of the dot is set as 100
+            ) # create a scatter plot
+plt.title("the effect of paternal age on offspring health")
 plt.xlabel("Paternal age")
 plt.ylabel("chilrden's health")
 plt.show()
-#now it will function correctly and output a plot with keys and values.   
+#now it will function correctly and output a plot with keys and values.
+age = int(input('Input the age of father:'))
+i = 0
+while True:
+    if paternal_age[i] == age:
+        print('When the father is',age, ', the risk of CHD in the offspring is', chd[i])
+        break
+    i += 1
+    if i > 10: # the loop will repeat for 10 times
+        break
+
--- Practical6/boxplot.py
+++ Practical6/boxplot.py
@@ -1,6 +1,31 @@
+# Pseudocode:
+# import pyplot and numpy
+# input the score
+# make chart
+
 import numpy as np
 import matplotlib.pyplot as plt
 
-score= [45,36,86,57,53,92,65,45]
-plt.boxplot(score,vert=True)
-plt.show()
+scores= [45,36,86,57,53,92,65,45]
+print(sorted(scores)) # print the score list in which the values are rearranged from low score to high score.
+plt.title("A boxplot for Rob's marks") # set the title of the plot
+plt.xlabel("Rob") # It is Rob's score
+plt.ylabel("marks") # It is a distribution of marks
+plt.boxplot(scores,
+            vert=True, # make the box vertical
+            patch_artist = True # fill the box with colour (blue)
+            )
+plt.show() # output the boxplot
+def mean(score): # this function can calculate the mean of values obtained from a list
+    sum = 0
+    mean = 0
+    for i in range(len(score)): # use a for-loop to sum up the values
+        sum += score[i]
+    mean = sum/(len(score))
+    return mean
+
+if mean(scores) < 60:
+    print('Rob failed the ICA')
+else:
+    print('Rob passed the ICA')
+# comment: Rob didn't pass the ICA.
