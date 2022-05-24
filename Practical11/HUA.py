5cc334f4b929be72c3a503daa4cbdf98a703fae9
resffffff <101092328+resffffff@users.noreply.github.com>
2022-05-25 03:05:38 +0800

    Delete HUA.py

--- Practical11/HUA.py
+++
@@ -1,5 +0,0 @@
-import re
-def replaceStr(oldStr, char, index):  # 在task1-4中均有使用的string替换方法
-    newStr = oldStr[:index] + char + oldStr[index+1:]
-    return newStr
-print(replaceStr('AGTTT','T',1,))
