e091ecedece07f189f3e565e80b22b726db3d3bd
resffffff <101092328+resffffff@users.noreply.github.com>
2022-05-25 02:03:18 +0800

    Practical 10

--- Practical10/ZJE human resources database.py
+++ Practical10/ZJE human resources database.py
@@ -1,4 +1,4 @@
-class staff():
+class Staff():
     def __init__(self, first_name, last_name, location, role):
         self.first_name = first_name
         self.last_name = last_name
@@ -7,5 +7,5 @@ class staff():
     def check(self):
         return "His/Her full name is %s %s.\nHe/She is working in %s.\nThe role of him/her is %s." %(self.first_name, self.last_name, self.location, self.role)
 # check. Log in the name of a student called Morty Race
-staff1 = staff(first_name='Morty', last_name='Rice', location='the university of Edinburgh', role='student')
+staff1 = Staff(first_name='Morty', last_name='Rice', location='the university of Edinburgh', role='student')
 print(staff1.check())
