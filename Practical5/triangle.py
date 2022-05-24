e31244d9b5348e426dcce51c0da5cb0b8e3bc074
resffffff <101092328+resffffff@users.noreply.github.com>
2022-05-24 18:01:47 +0800

    Update triangle.py

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,9 +1,9 @@
-#we know the sequence is p=(i*(i+1))/2.
-#p is the count of the sequence
-#i is the number of slice
-# the loop began when i equaled 1.
-# and finished when i reached 10.
-for i in range(1,11):
-    p=(i*(i+1))/2
-    print(p)
-# output all the p.
+# we know the sequence is p=(i*(i+1))/2.
+# sum is the amount of the triangle
+# i is the number of slice
+# the loop began when i equaled 1
+# and finished when i reached 10
+for i in range(1,11): # repeat the loop for 10 times
+    sum = (i*(i+1))/2
+    print(sum)
+# output the the number of triangles.
